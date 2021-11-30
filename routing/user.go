728746cf1888c6bf90375c1b69ca74f646b7c9dc
useralexey2000 <useralexey2000@gmail.com>
2021-11-30 19:54:08 +0200

    change routing/user.go form.has

--- routing/user.go
+++ routing/user.go
@@ -4,11 +4,16 @@ import (
 	"net/http"
 	"strconv"
 
+<<<<<<< Updated upstream
 	model "Dp218Go/domain/entities"
 	iface "Dp218Go/domain/interfaces"
 	repo "Dp218Go/repositories"
 
 	"github.com/go-chi/render"
+=======
+	"Dp218Go/services"
+
+>>>>>>> Stashed changes
 	"github.com/gorilla/mux"
 )
 
@@ -67,7 +72,21 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAllUsers(w http.ResponseWriter, r *http.Request) {
+<<<<<<< Updated upstream
 	users, err := userRepo.GetAllUsers()
+=======
+	var users = &models.UserList{}
+	var err error
+	format := GetFormatFromRequest(r)
+
+	r.ParseForm()
+	searchData := r.FormValue("SearchData")
+	if len(searchData) == 0 {
+		users, err = userService.GetAllUsers()
+	} else {
+		users, err = userService.FindUsersByLoginNameSurname(searchData)
+	}
+>>>>>>> Stashed changes
 	if err != nil {
 		render.Render(w, r, ServerErrorRenderer(err))
 		return
@@ -134,8 +153,75 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+<<<<<<< Updated upstream
 	if err := render.Render(w, r, &userData); err != nil {
 		render.Render(w, r, ServerErrorRenderer(err))
 		return
 	}
+=======
+
+	EncodeAnswer(format, w, &userWithRoleList{userData}, HTMLPath+"user-edit.html")
+}
+
+func allUsersOperation(w http.ResponseWriter, r *http.Request) {
+	format := GetFormatFromRequest(r)
+
+	r.ParseForm()
+	if _, ok := r.Form["ActionType"]; !ok {
+		return
+	}
+	actionType := r.FormValue("ActionType")
+	switch actionType {
+	case "BlockUser":
+		userId, err := strconv.Atoi(r.FormValue("UserID"))
+		if err != nil {
+			EncodeError(format, w, ErrorRendererDefault(err))
+			return
+		}
+		err = userService.ChangeUsersBlockStatus(userId)
+		if err != nil {
+			EncodeError(format, w, ErrorRendererDefault(err))
+			return
+		}
+	default:
+		EncodeError(format, w, ErrorRendererDefault(fmt.Errorf("unknown users operation")))
+	}
+	getAllUsers(w, r)
+}
+
+func DecodeUserUpdateRequest(r *http.Request, data interface{}) error {
+
+	var err error
+	r.ParseForm()
+	//userData := models.User{}
+	userData := data.(*models.User)
+
+	if _, ok := r.Form["LoginEmail"]; ok {
+		userData.LoginEmail = r.FormValue("LoginEmail")
+
+	}
+	if _, ok := r.Form["UserName"]; ok {
+		userData.UserName = r.FormValue("UserName")
+	}
+	if _, ok := r.Form["UserSurname"]; ok {
+		userData.UserSurname = r.FormValue("UserSurname")
+	}
+	if _, ok := r.Form["RoleID"]; ok {
+		var roleId int
+		roleId, err = strconv.Atoi(r.FormValue("RoleID"))
+		if err != nil {
+			return err
+		}
+		userData.Role, err = userService.GetRoleById(roleId)
+		if err != nil {
+			return err
+		}
+	}
+	if _, ok := r.Form["IsBlocked"]; ok {
+		userData.IsBlocked, _ = strconv.ParseBool(r.FormValue("IsBlocked"))
+	}
+
+	//reflect.ValueOf(data).Elem().Set(reflect.ValueOf(userData))
+	return nil
+>>>>>>> Stashed changes
 }
