bc0544c3cf42fd63b251ffb0009cf5dd167955d4
goRoman1 <git config --global user.email>
2021-12-01 12:14:04 +0200

    db connecting in struct

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"Dp-218_Go/configs"
 	"Dp-218_Go/server"
 )
 
 func main() {
-	server.Run()
+	cfg := configs.Get()
+	err := server.Run(cfg)
+	if err != nil {
+		return 
+	}
 }
 
